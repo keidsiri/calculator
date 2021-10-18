3bcd7260b1c9fa45f5766cc50dc47ea0dfb7117c
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-18 11:27:22 -0700

    Adding more functions: subtract,mutilply, divide
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-function add(num1, num2 ) {
+function add(num1, num2) {
   return num1 + num2 ;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide (num1 , num2) {
+  return num1 / num2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
